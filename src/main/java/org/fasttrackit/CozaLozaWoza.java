909593607f96b3bd86b66b93a1e5b26cae6fd9f6
LacoAncaLaura <laco.ancalaura@yahoo.com>
2020-04-06 21:43:22 +0300

    Pragmatic Exercises!

--- src/main/java/org/fasttrackit/CozaLozaWoza.java
+++ src/main/java/org/fasttrackit/CozaLozaWoza.java
@@ -1,15 +1,13 @@
 package org.fasttrackit;
 
 public class CozaLozaWoza {
-
     public static void main(String[] args) {
         for (int i = 1; i <= 110; i++) {
             for (int j = 11; j >= 1; j--) {
                 if (j >= 1) {
                     System.out.println(j);
                 }
-            }
-            if (i % 3 == 0 && i % 5 == 0) {
+            }   if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("CozaLoza");
                 } else if (i % 3 == 0 && i % 7 == 0) {
                     System.out.println("CozaLoza");
