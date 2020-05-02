8295248a4e734160418dbb5d1b82b541c9a28045
LacoAncaLaura <laco.ancalaura@yahoo.com>
2020-05-02 20:23:54 +0300

    Pragmatic Exercises!

--- src/main/java/org/fasttrackit/calculations/PaintCalculator.java
+++ src/main/java/org/fasttrackit/calculations/PaintCalculator.java
@@ -10,11 +10,10 @@ public class PaintCalculator {
             Scanner scanner = new Scanner(System.in);
             int squareFeet = scanner.nextInt();
             double squareMeters = squareFeet * 0.09290304;
-            for (int i = 0; 350 <= squareFeet; i++) {
-                System.out.println("You will need to purchase " + (i+1) + " gallons of paint to cover " + squareFeet + " square feet.\nOr " + squareMeters + " square meters.");
-                if (squareFeet < 350) {
-                    System.out.println("You will need to purchase only one gallon for " + squareFeet + " square feet.\nOr " + squareMeters + " square meters.");
-                }
+            int paint = squareFeet / 350;
+            System.out.println("You will need to purchase " + (paint + 1) + " gallons of paint to cover " + squareFeet + " square feet.\nOr " + squareMeters + " square meters.");
+            if (squareFeet < 350) {
+                System.out.println("You will need to purchase only one gallon for " + squareFeet + " square feet.\nOr " + squareMeters + " square meters.");
             }
         } catch (InputMismatchException e) {
             throw new RuntimeException("Please give a valid number.");
