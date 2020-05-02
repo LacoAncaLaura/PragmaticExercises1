6553720e7735c3c76e5a1df22f4ccb3eef003e28
LacoAncaLaura <laco.ancalaura@yahoo.com>
2020-05-02 22:09:23 +0300

    Pragmatic Exercises!

---
+++ src/main/java/org/fasttrackit/calculations/CurrencyConversion.java
@@ -0,0 +1,21 @@
+package org.fasttrackit.calculations;
+
+import java.util.Scanner;
+
+
+public class CurrencyConversion {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many euros are you exchanging?");
+        int euros = scanner.nextInt();
+        System.out.println("Exchange rate: ");
+        double exchangeRate = scanner.nextDouble();
+        double dollars = euros * exchangeRate;
+        System.out.println(euros + " euros at an exchange rate of " + exchangeRate + " is "
+         + dollars + " U.S. dollars.");
+
+    }
+}
+
+
