dd7b69e8626f137fe1a80da3d40093cc422e2f0f
LacoAncaLaura <laco.ancalaura@yahoo.com>
2020-05-18 20:00:23 +0300

    Pragmatic Exercises!

--- src/main/java/org/fasttrackit/MakingDecisions/TemperatureConverter.java
+++ src/main/java/org/fasttrackit/MakingDecisions/TemperatureConverter.java
@@ -1,12 +1,20 @@
 package org.fasttrackit.MakingDecisions;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TemperatureConverter {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean choice = scanner.nextBoolean();
-        System.out.println("Press C to convert from Fahrenheit to Celsius.");
-        System.out.println("Press C to convert from Celsius to Fahrenheit.");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            int temperature = scanner.nextInt();
+            int celsius = scanner.nextInt();
+            int fahrenheit = scanner.nextInt();
+            System.out.println("Press F to convert from Fahrenheit to Celsius.");
+            System.out.println("Press C to convert from Celsius to Fahrenheit.");
+            if (temperature.equals(celsius)){}
+        }catch (InputMismatchException e){
+            throw new RuntimeException("Please enter a valid number.");
+        }
     }
 }
