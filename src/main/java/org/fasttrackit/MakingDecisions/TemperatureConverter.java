dbcabadeb1e8d80906a1e3f6ae4eeaade5682c37
LacoAncaLaura <laco.ancalaura@yahoo.com>
2020-05-19 15:12:15 +0300

    Pragmatic Exercises!

--- src/main/java/org/fasttrackit/MakingDecisions/TemperatureConverter.java
+++ src/main/java/org/fasttrackit/MakingDecisions/TemperatureConverter.java
@@ -9,26 +9,41 @@ public class TemperatureConverter {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Press C to convert from Celsius to Fahrenheit.");
             String Celsius = "C".toLowerCase();
-            boolean isCelsius = "C".equalsIgnoreCase("c");
+//            boolean isCelsius = "C".equalsIgnoreCase("c");
             System.out.println("Press F to convert from Fahrenheit to Celsius.");
             String Fahrenheit = "F".toLowerCase();
-            boolean isFahrenheit = "F".equalsIgnoreCase("f");
+//            boolean isFahrenheit = "F".equalsIgnoreCase("f");
+            System.out.println("Press K to convert from Kelvin to Celsius.");
+            String Kelvin = "K".toLowerCase();
+//            boolean isKelvin = "K".equalsIgnoreCase("k");
+            System.out.println("Press J to convert from Kelvin to Fahrenheit.");
+            String Kelvin1 = "J".toLowerCase();
+//            boolean isKelvin1 = "J".equalsIgnoreCase("j");
             String choice = scanner.nextLine();
             System.out.println("And your choice is: " + choice);
             if (choice.equals(Celsius)) {
                 System.out.println("Please enter the temperature in Fahrenheit: ");
                 int temperature = scanner.nextInt();
-                double first = 5 / 9;
                 int second = temperature - 32;
-                double C = second * first;
+                double C = second * 0.56;
                 System.out.println(" The temperature in Celsius is: "  + C);
             } else if (choice.equals(Fahrenheit)) {
                 System.out.println("Please enter the temperature in Celsius: ");
                 int temperature = scanner.nextInt();
-                double third = 9 / 5;
-                double forth = temperature * third;
-                double F = third + forth;
+                double forth = temperature * 1.8;
+                double F = 1.8 + forth;
                 System.out.println( "The temperature in Fahrenheit is: " + F);
+            }else if (choice.equals(Kelvin)) {
+                System.out.println("Please enter the temperature in Celsius:");
+                int temperature = scanner.nextInt();
+                double K = temperature + 273.16;
+                System.out.println("The temperature in Kelvin scale is: " + K);
+            } else if (choice.equals(Kelvin1)){
+                System.out.println("Please enter the temperature in Fahrenheit:");
+                int temperature = scanner.nextInt();
+                int sixth = temperature - 32;
+                double K1= 0.56 * sixth + 273;
+                System.out.println("The temperature in Kelvin scale is: " + K1);
             }
         } catch (InputMismatchException e) {
             throw new RuntimeException("Please enter a valid number.");
