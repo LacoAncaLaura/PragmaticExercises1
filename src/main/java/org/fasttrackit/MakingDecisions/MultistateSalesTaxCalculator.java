e0a3fa8a2d8a7e6a27ee573d34798ea4062ae4db
LacoAncaLaura <laco.ancalaura@yahoo.com>
2020-05-25 14:08:48 +0300

    Pragmatic Exercises!

--- src/main/java/org/fasttrackit/MakingDecisions/MultistateSalesTaxCalculator.java
+++ src/main/java/org/fasttrackit/MakingDecisions/MultistateSalesTaxCalculator.java
@@ -2,6 +2,6 @@ package org.fasttrackit.MakingDecisions;
 
 public class MultistateSalesTaxCalculator {
     public static void main(String[] args) {
-
+        System.out.println("What is the order amount?");
     }
 }
