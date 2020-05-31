0e05af6cf983c9e713f39e7a22d7febec100524b
LacoAncaLaura <lala14444444@yahoo.com>
2020-05-31 18:37:21 +0300

    Pragmatic Exercises!

--- src/main/java/org/fasttrackit/MakingDecisions/MultistateSalesTaxCalculator.java
+++ src/main/java/org/fasttrackit/MakingDecisions/MultistateSalesTaxCalculator.java
@@ -1,8 +1,29 @@
 package org.fasttrackit.MakingDecisions;
 
+import java.util.Scanner;
+
 public class MultistateSalesTaxCalculator {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("What is the order amount?");
+        int amount = scanner.nextInt();
+        System.out.println("What state do you live in?");
+        if (scanner.nextLine().equalsIgnoreCase("Wisconsin")){
+            System.out.println("Which county?");
+            if (scanner.nextLine().equalsIgnoreCase("Eau Claire")){
+                System.out.println("The tax is $0.50.");
+                double total1 = amount * 0.005;
+                System.out.println("The total is $" + total1);
+            }else if (scanner.nextLine().equalsIgnoreCase("Dunn")){
+                System.out.println("The tax is $0.04.");
+                double total2 = amount * 0.004;
+                System.out.println("The total is $" + total2);
+            }
+        }if (scanner.nextLine().equalsIgnoreCase("Illinois")){
+            System.out.println("You will be charged with 8% tax.");
+            double total3 = amount * 0.08;
+            System.out.println("The total is $" + total3);
+        }
 
     }
 }
